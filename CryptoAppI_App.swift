9468709ce95537d666e38a31a834ee356aaa7cfa
monica8823 <Bochi.Shu@student.uts.edu.au>
2025-05-03 15:10:41 +1000

    Initial commit from Xcode

---
+++ CryptoAppI_App.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppI_App.swift
+//  CryptoAppÍ
+//
+//  Created by King Kong Trailer on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
